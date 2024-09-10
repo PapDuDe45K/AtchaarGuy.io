a7755f0edc81f926309a2552481ae350d15e4b56
Prior_Kiing <105079499+PapDuDe45K@users.noreply.github.com>
2024-09-10 14:01:37 +0200

    update aftr school

--- script.js
+++ script.js
@@ -4,46 +4,46 @@ showSlides(slideIndex);
 
 // Next/previous controls
 function plusSlides(n) {
-	showSlides(slideIndex += n);
+    showSlides(slideIndex += n);
 }
 
 // Thumbnail image controls
 function currentSlide(n) {
-	showSlides(slideIndex = n);
+    showSlides(slideIndex = n);
 }
 
 function showSlides(n) {
-	let i;
-	const slides = document.getElementsByClassName("slide");
-	const dots = document.getElementsByClassName("dot");
-	if (n > slides.length) {slideIndex = 1}
-	if (n < 1) {slideIndex = slides.length}
-	for (i = 0; i < slides.length; i++) {
-		slides[i].style.display = "none";
-	}
-	for (i = 0; i < dots.length; i++) {
-		dots[i].className = dots[i].className.replace(" active", "");
-	}
-	slides[slideIndex-1].style.display = "block";
-	dots[slideIndex-1].className += " active";
+    let i;
+    const slides = document.getElementsByClassName("slide");
+    const dots = document.getElementsByClassName("dot");
+    if (n > slides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = slides.length }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
 }
 
-// Hamburger menu script
+// Hamburger menu toggle
 const hamburger = document.getElementById('hamburger');
 const navMenu = document.getElementById('nav-menu');
 
 hamburger.addEventListener('click', () => {
-	navMenu.classList.toggle('active');
+    navMenu.classList.toggle('active');
 });
 
-// Footer Animation on Scroll
+// Footer animation on scroll
 const footer = document.querySelector('footer');
 
 function checkFooterInView() {
-	const rect = footer.getBoundingClientRect();
-	if (rect.top <= window.innerHeight) {
-		footer.classList.add('visible');
-	}
+    const rect = footer.getBoundingClientRect();
+    if (rect.top <= window.innerHeight) {
+        footer.classList.add('visible');
+    }
 }
 
 window.addEventListener('scroll', checkFooterInView);
